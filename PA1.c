14e01a3229eaae123f1df6b02618e95cb99d4e2e
Caroline Teicher <CarolineTeicher@Caroline-Teichers-MacBook-Pro.local>
2017-02-20 18:52:58 -0500

    messy code

--- PA1.c
+++ PA1.c
@@ -28,29 +28,81 @@
 #include <stdlib.h>
 
 int RAND_MAX = 1;
+int n; // number of total vertices in the graph
 
-int MSTaverage(int graph[v][v]){
-    bool visited[v];
-    int unvisited[v];
-    int vertStart;
-    int verEnd;
+// input pointer to two-dimensional array 
+int primAlgo(int pointer){
+    int visited[v];
+    int distance[v];
+    int min = 999;
+    int nodeco1;
+    int nodeco2;
+    int total;
+    int s = 0; // random value within appropriate range;
+    // int mincost;
+   
+    // int curr;
+  //  int i = length of vertices;
 
-    // intialize every vertex visited to false;
-    for (int i =0; i <vertices.length(); i++){
-        visited[i] = false;
+    // begin with empty spanning tree 
+    // initialize keys for vertices as infinity 
+    // int edges[v-1];
+
+    for (int i=0;i<v;i++){
+        visited[i] = 0;
+        distance[i] = INFINITY;
     }
 
-    while i < vertices{
-        if veritices have not been visited 
-            mark as visited
-            extract minimum edge and put on prioqirty que 
+    // first vertex picked first so initalize to 0
+    distance[s] = 0;
 
-            if min edge leads to unviisted edge
-                add to MST 
-                continue traversing 
+    //find vertices not visited and push its edges onto priorityqueue,
+    //find min edge and if it leads to an unviisted vertex then add 
+    //otherwise take min edge from priorityqueue again 
+    
+    // while visited does not contain all vertices
+    // find cut and pick minimum edge weight
 
+    while priorityqueue(heap) is not empty{
+        if visited[i] == 1{
+           pop min elemt in priorityqueue to get min weight
+                looking at neighborhind nodes - using adjventcy list .Search through lsits that has has one of vertices
+                for each vertex, copute edge wegiths bewween with distnace total and if curr is bigger, update with 
+                    just calc distance to sum
+
+                if (min > distance[i]){
+                    min = distance[i];
+                    nodeco1 = i;
+                    nodeco2 = i+1 || i-1;
+                }
+            }
+            //add edge to priorityqueue or adjlist
+            priorityqueue.enqueue(visited[i]);
+            min = priorityqueue.extractMin();
+            if createCycle(min) == true{
+                priorityqueue.delete(min){
+            }
+        
+            else
+                MST[curr] = min;
+                curr++;
+            }
+            i++;
+        }
+        else 
+            min = priorityqueue.extractMin();
+            if !visited[curr]{
+                MST[curr] = addE;
+            }
     }
-    int average = weight / graph.length();
+
+    int average;
+    
+    for (int i=0; i<MSTNodes.length();i++){
+        average = average + MST[i];
+    }
+
+    average = average/MSTnodes.length();
 
 }
 
@@ -64,9 +116,22 @@ struct edge{
     int weight;
 }
 
+struct graph{
+    int vertices;
+    int edges;
+
+}
+
+bool createCycle(edge){
+    if visited
+    return false;
+    else 
+        return true;
+}
 int calEdgeWeight(){
 
-// given a random value generate random weight edge 
+    //given a random value generate random weight edge 
     srand(time(NULL));
     int r = rand(); 
+    return r;
 }
